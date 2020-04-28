66d17be112b2fdea31f51a62a0f6834bda7b9668
Ackermannn <49069545+Ackermannn@users.noreply.github.com>
2020-04-28 16:28:39 +0800

    1.稳定时间段的滞后分析 2.整理代码文件

--- legacy/temp.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 22 22:23:53 2020
-
-@author: Administrator
-"""
-import pandas as pd
-file = '西昌2#高炉采集数据表_高炉本体(炉顶,炉喉,炉身,炉腹).pkl'
-df = pd.read_pickle('./'+file)
